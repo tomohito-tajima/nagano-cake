553d2460fa90ca2eb582e8a22df855b7c6d2a978
tomohito tajima <tomo322017@gmail.com>
2022-01-10 03:01:15 +0000

    add ルーティング・top画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #管理者側
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
+  #顧客者側
+  # URL /customers/sign_in ...
+  devise_for :customers, skip: [:passwords], controllers: {
+  registrations: "customer/registrations",
+  sessions: 'customer/sessions'
+  }
+
+  namespace :admin do
+    get '/admin' => 'homes#top'
+    resources :items, :except => :destroy
+    resources :genres, :except => [:new, :show, :destroy]
+    resources :customers, :except => [:new, :create, :destroy]
+    resources :orders, :only => [:show, :update]
+    resources :order_items, :only => :update
+  end
+
 end
