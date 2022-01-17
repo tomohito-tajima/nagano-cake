2b56a525e2703e69500633170059ca1c3629865c
tomohito tajima <tomo322017@gmail.com>
2022-01-17 09:54:31 +0000

    fix devise 新規登録

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,34 @@
 class ApplicationController < ActionController::Base
-  private
-  def after_sign_in_path_for(resource)
-    mypage_root_path # ログイン後に遷移するpathを設定
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  before_action :authenticate_admin!, if: :admin_url #pathに/adminが含まれている全てのページは、adminでログインしないと見れない
+
+  #request.fullpathで、飛んだ先のpathを全て取得します。
+  #include?("/admin")で、valと同じ要素が含まれていれば、trueを返します。
+  def admin_url
+    request.fullpath.include?("/admin")
   end
 
-  def after_sign_out_path_for(resource)
-    new_user_session_path # ログアウト後に遷移するpathを設定
+  def after_sign_in_path_for(resource)
+    if customer_signed_in?
+      customers_current_customer_path # ログイン後に遷移するpathを設定
+    elsif admin_signed_in?
+      admin_orders_path
+    end
+  end
+
+  def after_sign_out_path_for(resource) # ログアウト後に遷移するpathを設定
+    if resource == :customer
+      root_path
+    elsif resource == :admin
+      new_admin_session_path
+    end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :is_deleted])
   end
 end
--- app/controllers/customer/homes_controller.rb
+++ app/controllers/customer/homes_controller.rb
@@ -1,7 +1,2 @@
 class Customer::HomesController < ApplicationController
-  def top
-  end
-
-  def about
-  end
 end
