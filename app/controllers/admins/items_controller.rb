36d7d9485ce36d4bc925048a9d1e8c8abd6439a3
tomohito tajima <tomo322017@gmail.com>
2022-01-25 14:43:50 +0000

    add admin/items/new

---
+++ app/controllers/admins/items_controller.rb
@@ -0,0 +1,34 @@
+class Admins::ItemsController < ApplicationController
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_item_path(@item)
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :genre, :proce, :is_active)
+  end
+
+end
