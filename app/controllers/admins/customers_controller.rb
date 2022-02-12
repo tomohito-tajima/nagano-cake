c0cd877ca09bee70c2ff370974136b1db204463e
tomohito tajima <tomo322017@gmail.com>
2022-02-12 06:35:07 +0000

    add admins/customers/items/コントローラ

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -1,2 +1,21 @@
 class Admins::CustomersController < ApplicationController
+  
+  def new
+  end
+  
+  def create
+  end
+  
+  def index
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
 end
