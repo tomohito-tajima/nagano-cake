bd62f9eb49747187c7f7187f0fe4baa7ba93e702
tomohito tajima <tomo322017@gmail.com>
2022-02-13 04:57:03 +0000

    add admins/customers コントローラ

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -1,9 +1,13 @@
 class Admins::CustomersController < ApplicationController
   
   def new
+    @customer = Customer.new
   end
   
   def create
+    @customer = Customer.new(customer_params)
+    @customer.save
+    redirect_to admins_customer_path(@customer)
   end
   
   def index
