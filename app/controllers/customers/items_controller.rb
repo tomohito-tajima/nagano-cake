82c3ae05369aae7dee4f2e6fdd6b4fcd16749535
tomohito tajima <tomo322017@gmail.com>
2022-03-27 07:02:57 +0000

    add items・customer/コントローラ

--- app/controllers/customers/items_controller.rb
+++ app/controllers/customers/items_controller.rb
@@ -1,2 +1,11 @@
 class Customers::ItemsController < ApplicationController
+  def index
+    @genres = Genre.all
+    @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+    @cart_item = CartItem.new
+  end
 end
